2c2e93b4d0e8ce0af1475e73333a40e5582a55ea
Clark Manuntag <camanuntag@student.hau.edu.ph>
2024-03-09 23:08:44 +0800

    Mar. 9, 2024 Latest

--- react_app/src/screens/Dashboard.js
+++ react_app/src/screens/Dashboard.js
@@ -1,125 +1,115 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../actions/userActions";
-import pic from "../images/dazzling.jpg";
-import pic2 from "../images/wp3.jpg";
-import "../styles.css";
+import React from "react";
+import "./Dashboard.css";
+import avengerImage from "../images/avenger.jpg";
+import sevenSundaysImage from "../images/7sundays.jpg";
+import zohanImage from "../images/zohan.jpg";
+import pic from "../images/bg.jpg";
+import a from "../images/poster2.png"
+import q from "../images/poster3.png"
+import w from "../images/poster4.png"
+import e from "../images/poster5.png"
+import r from "../images/poster6.png"
+import t from "../images/poster7.png"
+import y from "../images/poster8.png"
+import s from "../images/poster9.png"
+import d from "../images/poster10.png"
+import f from "../images/poster11.png"
+import g from "../images/poster12.png"
 
-function Dashboard() {
-  const userLogin = useSelector((state) => state.userLogin);
-  const { error, loading, userInfo } = userLogin;
 
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+const movies = [
+  {  image: zohanImage, link: "https://ww16.0123movie.net/movie/you-dont-mess-with-the-zohan-6396.html" },
+  {  image: avengerImage, link: "https://ww16.0123movie.net/movie/the-avengers-1717.html" },
+  {  image: sevenSundaysImage, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: a, link: "https://ww16.0123movie.net/movie/mulan-2009-16943.html" },
+  {  image: q, link: "https://ww16.0123movie.net/movie/The-Falcon-Winter-Soldier-1-12101099.html" },
+  {  image: w, link: "https://ww16.0123movie.net/movie/avengers-endgame-28501.html" },
+  {  image: e, link: "https://ww16.0123movie.net/movie/loki-season-2-1630855858.html" },
+  {  image: r, link: "https://ww16.0123movie.net/movie/thor-ragnarok-22466.html" },
+  {  image: t, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: y, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: s, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: d, link: "https://ww16.0123movie.net/movie/soul-101917.html" },
+  {  image: f, link: "https://ww16.0123movie.net/movie/thor-ragnarok-22466.html" },
+  {  image: g, link: "https://ww16.0123movie.net/movie/loki-season-2-1630855858.html" },
+  {  image: zohanImage, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: avengerImage, link: "https://ww16.0123movie.net/movie/thor-ragnarok-22466.html" },
+  {  image: sevenSundaysImage, link: "https://ww16.0123movie.net/movie/easter-sunday-1630853957.html" },
+  {  image: a, link: "https://ww16.0123movie.net/movie/mulan-2009-16943.html" },
+  {  image: q, link: "https://ww16.0123movie.net/movie/The-Falcon-Winter-Soldier-1-12101099.html" },
+  {  image: w, link: "https://ww16.0123movie.net/movie/avengers-endgame-28501.html" },
+  {  image: e, link: "https://ww16.0123movie.net/movie/loki-season-2-1630855858.html" },
+  {  image: r, link: "https://ww16.0123movie.net/movie/thor-ragnarok-22466.html"  },
+  {  image: t, link: "https://ww16.0123movie.net/movie/soul-101917.html" },
+  {  image: y , link: "https://ww16.0123movie.net/movie/soul-101917.html"},
+  {  image: s , link: "https://ww16.0123movie.net/movie/soul-101917.html"},
+];
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate(); // Import useNavigate
+const MovieCard = ({ title, genre, image, link }) => (
+  <a href={link} style={{ textDecoration: "none" }}>
+    <div className="movieCard">
+      <img className="movieImage" src={image} alt={`${title} Poster`} />
+      <h2>{title}</h2>
+      <p>{genre}</p>
+    </div>
+  </a>
+);
+const Dashboard = () => (
+  <div>
+    <header style={{ backgroundColor: "black", padding: "5px", display: "flex", justifyContent: "space-between" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h1 style={{ color: "blue" }}>In-Bang</h1>
+        <nav style={{ marginLeft: "20px" }}>
+          <ul style={{ listStyle: "none", display: "flex", alignItems: "center" }}>
+            <li style={{ marginRight: "10px" }}>
+              <a href="/Movie" style={{ color: "white", textDecoration: "none" }}>Movie</a>
+            </li>
+            <li style={{ marginRight: "10px" }}>
+              <a href="/TV shows" style={{ color: "white", textDecoration: "none" }}>TV shows</a>
+            </li>
+            <li style={{ marginRight: "10px" }}>
+              <a href="/K-Drama" style={{ color: "white", textDecoration: "none" }}>K-Drama</a>
+            </li>
+            <li>
+              <a href="/Disney" style={{ color: "white", textDecoration: "none" }}>Disney</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <a className="custom-button" href="/" style={{ backgroundColor: "#f44336", color: "white", border: "none", padding: "5px 10px", borderRadius: "5px", cursor: "pointer", alignSelf: "flex-start" }}>
+        Log out
+      </a>
+    </header>
+    {/* Rest of the component */}
 
-  const logoutHandler = () => {
-    dispatch(logout());
-    navigate("/"); // Redirect to the index path upon logout
-  };
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const handleChangePassword = () => {
-    handleCloseUserMenu(); // Close the user menu
-    navigate("/change-password"); // Navigate to the change password page
-  };
-
-  return (
     <div
       style={{
-        position: "relative",
-        overflow: "hidden",
+        content: "",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        opacity: "24%",
+        backgroundImage: `url(${pic})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        zIndex: -1,
       }}
-    >
-      <div style={{ margin: "24px", overflowY: "auto", height: "100vh" }}>
-        <div style={{ position: "relative" }}>
-          <div className="carousel">{/* Carousel images */}</div>
-          <div className="video-cards">{/* Video cards */}</div>
-          <div className="card-sliders">{/* Card sliders */}</div>
-          <div className="code-structure">{/* Code structure */}</div>
+    />
 
-          <div className="content">
-            {/* Add your content here */}
-            <Link to="/login">
-              <button
-                style={{
-                  cursor: "pointer",
-                  border: "2px solid rgba(18, 0, 184, 1)",
-                  backgroundColor: "rgba(18, 0, 184, 1)",
-                  borderRadius: "5px",
-                  color: "#ffffff",
-                  padding: "10px",
-                  fontSize: "18px",
-                }}
-              >
-                Login
-              </button>
-            </Link>
-          </div>
-          <button
-            onClick={logoutHandler}
-            style={{
-              cursor: "pointer",
-              border: "2px solid rgba(18, 0, 184, 1)",
-              backgroundColor: "rgba(18, 0, 184, 1)",
-              borderRadius: "5px",
-              color: "#ffffff",
-              padding: "10px",
-              fontSize: "18px",
-              marginTop: "20px",
-            }}
-          >
-            Logout
-          </button>
-        </div>
-        <div style={{ position: "relative" }}>
-          <h1
-            style={{
-              color: "#004cff",
-              fontSize: "50px",
-              margin: 0,
-              padding: 0,
-            }}
-          >
-            인뱅
-          </h1>
-          <h2
-            style={{
-              color: "#004cff",
-              position: "absolute",
-              fontSize: "20px",
-              top: "40px",
-              left: "50px",
-              transform: "rotate(-24deg)",
-            }}
-          >
-            In-Bang
-          </h2>
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
+    <div id="movieContainer">
+      {movies.map((movie, index) => (
+        <MovieCard key={index} {...movie} />
+        
+      ))}
+      
     </div>
-  );
-}
+    <footer>
+      <p>&copy; 2024 In-Bang Dashboard</p>
+    </footer>
+  </div>
+);
 
 export default Dashboard;
